2727c342457168d1c3dbaa9107a2db86b5d79e6b
Michael Forster <email@michael-forster.de>
2023-07-16 15:31:09 +0200

    A few small cleanups

--- src/Calendar.tsx
+++ src/Calendar.tsx
@@ -8,8 +8,9 @@ const DAYS = 24 * 60 * 60 * 1000
 const DAY_FMT = new Intl.DateTimeFormat("de-DE", { dateStyle: "full" })
 
 function startOfWeek(date: Date): Date {
+  date.setHours(0, 0, 0, 0) // Go to start of day
   const day = (date.getDay() + 6) % 7 // Use Monday as first day of the week
-  return new Date(date.getTime() - day * DAYS)
+  return new Date(date.getTime() - day * DAYS) // Go to start of week
 }
 
 export default function Calendar() {
@@ -20,15 +21,14 @@ export default function Calendar() {
   const [data3, setData3] = useState<Data>()
 
   useEffect(() => {
-    const saal1 = "cafed7a0ea7ede3dc2abdcb1af7a8ce7de4a0348508655510ec673a6a2097ba0%40group.calendar.google.com"
-    const saal2 = "23f000f0d58614525d3995bad740adee45252d2381a5c7a25b2e7a6710ad48e8%40group.calendar.google.com"
-    const saal3 = "f8af0ec05dc660695eca9dfb723beb221ffd05369b4f0550eca64e2dac195442%40group.calendar.google.com"
+    const floor1 = "cafed7a0ea7ede3dc2abdcb1af7a8ce7de4a0348508655510ec673a6a2097ba0%40group.calendar.google.com"
+    const floor2 = "23f000f0d58614525d3995bad740adee45252d2381a5c7a25b2e7a6710ad48e8%40group.calendar.google.com"
+    const floor3 = "f8af0ec05dc660695eca9dfb723beb221ffd05369b4f0550eca64e2dac195442%40group.calendar.google.com"
 
-    fetchData(saal1, date).then(setData1).catch(console.error)
-    fetchData(saal2, date).then(setData2).catch(console.error)
-    fetchData(saal3, date).then(setData3).catch(console.error)
+    fetchData(floor1, date).then(setData1).catch(console.error)
+    fetchData(floor2, date).then(setData2).catch(console.error)
+    fetchData(floor3, date).then(setData3).catch(console.error)
   }, [date])
-
 
   return <>
     <div style={{ position: "fixed" }}>
@@ -48,9 +48,9 @@ export default function Calendar() {
           gridRow: hour * 4 + 2,
         }}>{hour + 8}:00</div>
       )}
-      {data1?.items.map((event) => <Event event={event} key={event.id} saal={1} />)}
-      {data2?.items.map((event) => <Event event={event} key={event.id} saal={2} />)}
-      {data3?.items.map((event) => <Event event={event} key={event.id} saal={3} />)}
+      {data1?.items.map(event => <Event event={event} key={event.id} floor={1} />)}
+      {data2?.items.map(event => <Event event={event} key={event.id} floor={2} />)}
+      {data3?.items.map(event => <Event event={event} key={event.id} floor={3} />)}
     </div>
   </>
 }
--- src/Event.tsx
+++ src/Event.tsx
@@ -3,12 +3,11 @@ import './Event.css'
 
 const TIME_FMT = new Intl.DateTimeFormat("de-DE", { timeStyle: "short" })
 
-export default function Event({ event, saal }: { event: EventData, saal: number }) {
+export default function Event({ event, floor }: { event: EventData, floor: number }) {
   const start = new Date(event.start?.dateTime)
   const end = new Date(event.end?.dateTime)
 
   let type = "unknown"
-
   if (event.summary.match(/(Std|Standard)/i)) { type = "standard" }
   if (event.summary.match(/(Lat|Latein)/i)) { type = "latein" }
   if (event.summary.match(/Breitensport/i)) { type = "breitensport" }
@@ -16,7 +15,7 @@ export default function Event({ event, saal }: { event: EventData, saal: number 
   return <div className={`event ${type}`} style={{
     gridRowStart: start.getHours() * 4 + start.getMinutes() / 15 + 2 - 32,
     gridRowEnd: end.getHours() * 4 + end.getMinutes() / 15 + 2 - 32,
-    gridColumn: (start.getDay() + 6) % 7 * 3 + saal + 1,
+    gridColumn: (start.getDay() + 6) % 7 * 3 + floor + 1,
   }}>
     <header>{TIME_FMT.format(start)} – {TIME_FMT.format(end)}</header>
     <p title={event.summary}>{event.summary}</p>
